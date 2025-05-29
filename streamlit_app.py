5b59b604f022c016151e8d366c6a09e1d34b2292
JODRAFF <traoresierachid@gmail.com>
2025-05-29 23:28:36 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Configuration de la page Streamlit
 st.set_page_config(page_title="Prédiction du BANK CHURN ", layout="wide")
