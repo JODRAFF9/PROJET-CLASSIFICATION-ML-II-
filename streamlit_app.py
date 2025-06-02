f0ea6ed7f605a0349cc73f9f2756e4d8a2fb5bc4
JODRAFF <traoresierachid@gmail.com>
2025-06-02 01:52:27 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ if "page" not in st.session_state:
     st.session_state.page = "Accueil"
 
 # Titre de l'application
-st.title("> 🏡 **Application de Prédiction du BANK CHURN**")
+st.title("🏡 > **Application de Prédiction du BANK CHURN**")
 # Fonction pour changer la page active dans st.session_state
 def set_page(page_name):
     st.session_state.page = page_name
