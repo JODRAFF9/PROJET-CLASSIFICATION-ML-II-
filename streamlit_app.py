d74061ed19888e88c9fe292da57e3cd04369de71
JODRAFF <traoresierachid@gmail.com>
2025-06-05 16:56:05 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -218,7 +218,7 @@ elif st.session_state.page == "Analyse":
         st.plotly_chart(fig2, use_container_width=True)
 
     # 3. Age vs Churn
-    st.markdown("### 🎯 Âge et Churn")
+    st.markdown("### 🎯 la rélation entre âge et Churn")
     fig3 = px.box(train_df_labelled, x='Exited', y='Age', color='Exited', 
                 title='Répartition de l\'âge selon le statut (Churn)', 
                 color_discrete_map={0: "green", 1: "red"})
@@ -243,57 +243,63 @@ elif st.session_state.page == "Analyse":
         
     
     
-    """
-    if st.checkbox("Afficher les données brutes"):
-        st.dataframe(train_df.head(100))
+# """     """
+#     if st.checkbox("Afficher les données brutes"):
+#         st.dataframe(train_df.head(100))
 
-    st.write("### Statistiques descriptives")
-    st.write(train_df_labelled[num_cols].describe())
+#     st.write("### Statistiques descriptives")
+#     st.write(train_df_labelled[num_cols].describe())
 
-    st.write("### Visualisation de deux variables")
+#     st.write("### Visualisation de deux variables")
 
-    nomx = st.selectbox("Variable X", noms_descriptifs)
-    nomy = st.selectbox("Variable Y", noms_descriptifs)
+#     nomx = st.selectbox("Variable X", noms_descriptifs)
+#     nomy = st.selectbox("Variable Y", noms_descriptifs)
 
-    variable_x=nom_variable(nomx,mode="vers_technique")
-    variable_y=nom_variable(nomy,mode="vers_technique")
+#     variable_x=nom_variable(nomx,mode="vers_technique")
+#     variable_y=nom_variable(nomy,mode="vers_technique")
 
 
-    # Visualisation des relations entre les variables
-    fig, ax = plt.subplots(figsize=(10, 8))
-    if variable_x in num_cols and variable_y in num_cols:
-        sns.scatterplot(data=train_df_labelled, x=variable_x, y=variable_y, ax=ax, color="teal", s=100, edgecolor='black')
-        ax.set_title(f"Nuage de points entre {nomx} et {nomy}", fontsize=16, fontweight='bold')
-        ax.set_xlabel(variable_x, fontsize=14)
-        ax.set_ylabel(variable_y, fontsize=14)
-        ax.tick_params(axis='both', which='major', labelsize=12,rotation=45)
-        ax.grid(True, linestyle='--', alpha=0.7)
-    elif (variable_x in cat_cols or variable_x in target) and (variable_y in cat_cols or variable_y in target):
-        grouped_train_df_labelled = train_df_labelled.groupby([variable_x, variable_y]).size().unstack()
-        grouped_train_df_labelled.plot(kind='bar', stacked=True, ax=ax, cmap='coolwarm')
-        ax.set_title(f"Graphique en barres empilées de {nomx} par {nomy}", fontsize=16, fontweight='bold')
-        ax.set_xlabel(variable_x, fontsize=14)
-        ax.set_ylabel("Effectifs", fontsize=14)
-        ax.tick_params(axis='both', which='major', labelsize=12,rotation=45)
-        ax.legend(title=variable_y, fontsize=12)
-    else:
-        sns.boxplot(data=train_df_labelled, x=variable_x, y=variable_y, ax=ax, palette="Set2")
-        ax.set_title(f"Graphique de boîte de {nomy} par {nomx}", fontsize=16, fontweight='bold')
-        ax.set_xlabel(variable_x, fontsize=14)
-        ax.set_ylabel(variable_y, fontsize=14)
-        ax.tick_params(axis='both', which='major', labelsize=12,rotation=45)
+#     # Visualisation des relations entre les variables
+#     fig, ax = plt.subplots(figsize=(10, 8))
+#     if variable_x in num_cols and variable_y in num_cols:
+#         sns.scatterplot(data=train_df_labelled, x=variable_x, y=variable_y, ax=ax, color="teal", s=100, edgecolor='black')
+#         ax.set_title(f"Nuage de points entre {nomx} et {nomy}", fontsize=16, fontweight='bold')
+#         ax.set_xlabel(variable_x, fontsize=14)
+#         ax.set_ylabel(variable_y, fontsize=14)
+#         ax.tick_params(axis='both', which='major', labelsize=12,rotation=45)
+#         ax.grid(True, linestyle='--', alpha=0.7)
+#     elif (variable_x in cat_cols or variable_x in target) and (variable_y in cat_cols or variable_y in target):
+#         grouped_train_df_labelled = train_df_labelled.groupby([variable_x, variable_y]).size().unstack()
+#         grouped_train_df_labelled.plot(kind='bar', stacked=True, ax=ax, cmap='coolwarm')
+#         ax.set_title(f"Graphique en barres empilées de {nomx} par {nomy}", fontsize=16, fontweight='bold')
+#         ax.set_xlabel(variable_x, fontsize=14)
+#         ax.set_ylabel("Effectifs", fontsize=14)
+#         ax.tick_params(axis='both', which='major', labelsize=12,rotation=45)
+#         ax.legend(title=variable_y, fontsize=12)
+#     else:
+#         sns.boxplot(data=train_df_labelled, x=variable_x, y=variable_y, ax=ax, palette="Set2")
+#         ax.set_title(f"Graphique de boîte de {nomy} par {nomx}", fontsize=16, fontweight='bold')
+#         ax.set_xlabel(variable_x, fontsize=14)
+#         ax.set_ylabel(variable_y, fontsize=14)
+#         ax.tick_params(axis='both', which='major', labelsize=12,rotation=45)
 
-    st.pyplot(fig)
+#     st.pyplot(fig)
+#     st.write("---")
+
+#     st.write("### Matrice de Corrélation")
+#     correlation_matrix = train_df_labelled[num_cols].corr()
+#     mask = np.triu(np.ones_like(correlation_matrix, dtype=bool))
+#     fig_corr, ax_corr = plt.subplots(figsize=(14, 12))
+#     sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", mask=mask, fmt=".2f")
+#     st.pyplot(fig_corr)*/
+#     """ """
+# Section Accueil
+if st.session_state.page == "Prédiction":
     st.write("---")
-
-    st.write("### Matrice de Corrélation")
-    correlation_matrix = train_df_labelled[num_cols].corr()
-    mask = np.triu(np.ones_like(correlation_matrix, dtype=bool))
-    fig_corr, ax_corr = plt.subplots(figsize=(14, 12))
-    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", mask=mask, fmt=".2f")
-    st.pyplot(fig_corr)
-
-    st.header(" Description des donnée")
+    
+# Section Accueil
+if st.session_state.page == "A-propos":
+    st.header(" Description des données")
     file_path="docs/description.txt"
     
     try:
@@ -305,12 +311,6 @@ elif st.session_state.page == "Analyse":
 
     st.text_area("Aperçu de la description des données :", description, height=300)
     st.download_button("Télécharger la description des données", data=description, file_name="description.txt")
-    st.write("---") */
-    """
-# Section Accueil
-if st.session_state.page == "Prédiction":
+    st.write("---") 
     st.write("---")
     
-# Section Accueil
-if st.session_state.page == "A-propos":
-    st.write("---")
